54eac60889565a5cd7cf5fadda3b8ebc788666db
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2021-10-28 16:18:12 +0200

    added test
     - some simulations are run as test and the results are compared

---
+++ test/runtests.jl
@@ -0,0 +1,62 @@
+using Test, GasChromatographySimulator
+
+# Options
+opt = GasChromatographySimulator.Options(OwrenZen5(), 1e-6, 1e-3, "inlet", true)
+
+# System
+a_d = [0.25e-3]
+a_df = [0.25e-6]
+d(x) = GasChromatographySimulator.gf_const(x, a_d)
+df(x) = GasChromatographySimulator.gf_const(x, a_df)
+sys = GasChromatographySimulator.System(10.0, d, a_d, df, a_df, "SLB5ms", "He")
+
+# Program
+time_steps = [0.0, 60.0, 600.0, 300.0]
+temp_steps = [40.0, 40.0, 300.0, 300.0]
+pin_steps = [200.0, 200.0, 300.0, 300.0].*1000.0 .+ 101300
+pout_steps = [101.3, 101.3, 101.3, 101.3].*1000.0
+a_gf = [zeros(length(time_steps)) zeros(length(time_steps)) 10.0.*ones(length(time_steps)) zeros(length(time_steps))]
+gf(x) = GasChromatographySimulator.gf_const(x, a_gf)
+T_itp = GasChromatographySimulator.temperature_interpolation(time_steps, temp_steps, gf, sys.L)
+pin_itp = GasChromatographySimulator.pressure_interpolation(time_steps, pin_steps)
+pout_itp = GasChromatographySimulator.pressure_interpolation(time_steps, pout_steps)
+prog = GasChromatographySimulator.Program(time_steps, temp_steps, pin_steps, pout_steps, gf, a_gf, T_itp, pin_itp, pout_itp)
+
+# Substance from the load-function-> 1st test
+db_path = string(@__DIR__, "/data")
+db = "Database_test.csv"
+solutes = ["C10", "C11"]
+init_t = zeros(length(solutes))
+init_τ = zeros(length(solutes))
+sub = GasChromatographySimulator.load_solute_database(db_path, db, sys.sp, sys.gas, solutes, init_t, init_τ)
+
+# parameters
+par = GasChromatographySimulator.Par(sys, prog, sub, opt)
+
+# solving
+# for odesys:
+sol_odesys = GasChromatographySimulator.solve_system_multithreads(par)
+pl_odesys = GasChromatographySimulator.peaklist(sol_odesys, par)
+# test: tR(C10) = 97.943, τR(C10) = 0.581
+# test: tR(C11) = 140.416, τR(C11) = 0.540
+@test round.(pl_odesys.tR, digits=3) == [97.943, 140.416]
+@test round.(pl_odesys.τR, digits=3) == [0.581, 0.540]
+
+sol_odesys_ng = GasChromatographySimulator.solve_system_multithreads(par, ng=true)
+pl_odesys_ng = GasChromatographySimulator.peaklist(sol_odesys_ng, par)
+# non-gradient separation should give the 'same' results with both methods (ng=false and ng=true)
+@test abs.(1 .- pl_odesys_ng.tR./pl_odesys.tR)[1] < 1e-5
+@test abs.(1 .- pl_odesys_ng.τR./pl_odesys.τR)[2] < 1e-4
+
+# for migration and peakvariance separatly:
+sol_migr, sol_peak = GasChromatographySimulator.solve_multithreads(par)
+pl_migr = GasChromatographySimulator.peaklist(sol_migr, sol_peak, par)
+# test: tR(C10) = 97.944, τR(C10) = 0.578
+# test: tR(C11) = 140.403, τR(C11) = 0.540
+@test round.(pl_migr.tR, digits=3) == [97.944, 140.403]
+@test round.(pl_migr.τR, digits=3) == [0.578, 0.540]
+
+sol_migr_ng, sol_peak_ng = GasChromatographySimulator.solve_multithreads(par, ng=true)
+pl_migr_ng = GasChromatographySimulator.peaklist(sol_migr_ng, sol_peak_ng, par)
+@test abs.(1 .- pl_migr_ng.tR./pl_migr.tR)[1] < 1e-5
+@test abs.(1 .- pl_migr_ng.τR./pl_migr.τR)[2] < 1e-4
