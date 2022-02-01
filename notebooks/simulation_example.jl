0a6f13b07df05f00161e6db04c60261638fc42af
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2022-02-01 09:51:01 +0100

    Update simulation_example.jl

--- notebooks/simulation_example.jl
+++ notebooks/simulation_example.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.17.5
+# v0.17.7
 
 using Markdown
 using InteractiveUtils
@@ -21,9 +21,11 @@ begin
     Pkg.add([
         Pkg.PackageSpec(name="Plots", version="1"),
         Pkg.PackageSpec(name="PlutoUI", version="0.7"),
-		Pkg.PackageSpec(url="https://github.com/JanLeppert/GasChromatographySimulator.jl")
+		Pkg.PackageSpec(name="UrlDownload"),
+		Pkg.PackageSpec(url="https://github.com/JanLeppert/GasChromatographySimulator.jl"),
+	Pkg.PackageSpec(url="https://github.com/JanLeppert/GasChromatographyTools.jl")
     ])
-    using Plots, PlutoUI, GasChromatographySimulator
+    using Plots, PlutoUI, UrlDownload, GasChromatographySimulator, GasChromatographyTools
 	md"""
 	Packages
 	"""
@@ -59,13 +61,22 @@ md"""
 begin
 	solute_db_path = "../data"
 	solute_db = "Database_test.csv"
-	db = DataFrame(CSV.File(joinpath(solute_db_path, solute_db), header=1, silencewarnings=true))
+	db = DataFrame(urldownload("https://github.com/JanLeppert/GasChromatographySimulator.jl/raw/main/data/Database_test.csv"))
 	sp = unique(db.Phase)
 	md"""
 	### Solute Database
 	$(embed_display(db))
 	"""
 end
+
+# ╔═╡ e0669a58-d5ac-4d01-b079-05412b413dda
+@bind sys_values confirm(GasChromatographyTools.UI_System(sp))
+
+# ╔═╡ a7e1f0ee-714e-4b97-8741-d4ab5321d5e0
+@bind prog_values confirm(GasChromatographyTools.UI_Program())
+
+# ╔═╡ 3e053ac1-db7b-47c1-b52c-00e26b59912f
+@bind opt_values confirm(GasChromatographyTools.UI_Options())
 
 # ╔═╡ 323a769f-55f9-41dd-b8f1-db7928996a52
 md"""
@@ -81,145 +92,11 @@ md"""
 Plot $(@bind yy Select(["z", "t", "T", "τ", "σ", "u"]; default="t")) over $(@bind xx Select(["z", "t", "T", "τ", "σ", "u"]; default="z"))
 """
 
-# ╔═╡ 802e4071-b22b-4411-b589-205292aabc75
-# functions
-begin
-
-"""
-    sys_set_UI(sp)
-
-Construct a combined PlutoUI widget for the settings of the GC system with the selectable stationary phases `sp`.    
-"""
-function sys_set_UI(sp)
-	PlutoUI.combine() do Child
-		md"""
-		### System settings 
-		
-		``L`` [m]: $(
-			Child(NumberField(0.1:0.1:100.0; default=10.0))
-		) ``d`` [mm]: $(
-			Child(NumberField(0.01:0.01:1.00; default=0.25))
-		) ``d_f`` [µm]: $(
-			Child(NumberField(0.01:0.01:1.00; default=0.25))
-		) stat. phase: $(
-			Child(Select(sp; default="SLB5ms"))
-		) Gas: $(
-			Child(Select(["He", "H2", "N2"]; default="He"))
-		) 
-			
-		"""
-	end
-end
-
-"""
-    prog_set_UI(sp)
-
-Construct a combined PlutoUI widget for the settings of the program of a GC system.    
-"""
-function prog_set_UI()
-	PlutoUI.combine() do Child
-		md"""
-		### Program settings 
-		_Note: Same number of entrys for every text field._
-		
-		$(
-			Child(TextField((50,1); default="0 60 300 300 120"))
-		) time steps [s] 
-		
-		$(
-			Child(TextField((50,1); default="40 40 170 300 300"))
-		) temperature steps [°C]
-		
-		$(
-			Child(TextField((50,1); default="0 0 40 60 0"))
-		) ``ΔT`` steps [°C]
-		
-		$(
-			Child(TextField((50,1); default="-3 -3 -3 -3 -3"))
-		) ``α`` steps
-
-		$(
-			Child(TextField((50,1); default="18 18 58 98 98"))
-		) ``p_{in}`` steps [kPa(g)]
-
-		$(
-			Child(TextField((50,1); default="0 0 0 0 0"))
-		)``p_{out}`` steps [kPa(a)]
-			
-		"""
-	end
-end
-
-"""
-    sub_set_UI(sp)
-
-Construct a combined PlutoUI widget for the settings of the substances separated in the simulated GC system with the selectable substances `subs`.    
-"""
-function sub_set_UI(sol)
-	if length(sol)>10
-		select_size = 10
-	else
-		select_size = length(sol)
-	end
-	PlutoUI.combine() do Child
-		md"""
-		### Substance settings 
-		
-		Select Substances: $(
-			Child(MultiSelect(sol; default=sol[1:4], size=select_size))
-		) 
-		
-		Injection time [s]: $(
-			Child(NumberField(0.0:0.1:100.0; default=0.0))
-		) and Injection width [s]: $(
-			Child(NumberField(0.00:0.01:10.0; default=0.0))
-		) 
-		"""
-	end
-end
-
-"""
-    opt_set_UI(sp)
-
-Construct a combined PlutoUI widget for the settings of the options for the simulation.    
-"""
-function opt_set_UI()
-	PlutoUI.combine() do Child
-		md"""
-		### Option settings 
-		
-		abstol: 1e $(
-			Child(NumberField(-10:1:-3; default=-8))
-		) reltol: 1e $(
-			Child(NumberField(-8:1:-2; default=-5))
-		) Tcontrol: $(
-			Child(Select(["inlet", "outlet"]; default="inlet"))
-		)
-		"""
-	end
-end
-
-
-
-	md"""
-	Definition of UI functions.
-	"""
-end
-
-# ╔═╡ e0669a58-d5ac-4d01-b079-05412b413dda
-@bind sys_values confirm(sys_set_UI(sp))
-
-# ╔═╡ a7e1f0ee-714e-4b97-8741-d4ab5321d5e0
-@bind prog_values confirm(prog_set_UI())
-
-# ╔═╡ 3e053ac1-db7b-47c1-b52c-00e26b59912f
-@bind opt_values confirm(opt_set_UI())
-
 # ╔═╡ f7f06be1-c8fa-4eee-953f-0d5ea26fafbf
 sys = GasChromatographySimulator.System(sys_values[1], sys_values[2]*1e-3, sys_values[3]*1e-6, sys_values[4], sys_values[5]);
 
 # ╔═╡ 7a00bb54-553f-47f5-b5db-b40d226f4183
-@bind sub_values confirm(sub_set_UI(GasChromatographySimulator.all_solutes(sys.sp, db)))
+@bind sub_values confirm(GasChromatographyTools.UI_Substance(GasChromatographySimulator.all_solutes(sys.sp, db)))
 
 # ╔═╡ e3277bb4-301a-4a1e-a838-311832b6d6aa
 sub = GasChromatographySimulator.load_solute_database(db, sys.sp, sys.gas, sub_values[1], sub_values[2].*ones(length(sub_values[1])), sub_values[3].*ones(length(sub_values[1])));
@@ -274,86 +151,6 @@ md"""
 $(embed_display(peaklist))
 """
 
-# ╔═╡ 48f91bc4-35ce-470a-9b6d-eb3c08da27dc
-begin
-	
-function add_plots(xx, yy, sol, par)
-	n = size(sol)[1]
-
-	df_sol = GasChromatographySimulator.sol_extraction(solution, par)
-	xvalues = Array{Array{Float64,1}}(undef, n)
-	yvalues = Array{Array{Float64,1}}(undef, n)
-	
-	p_add = plot(legend=false)
-	for i=1:n
-		if xx=="z"
-			xvalues[i] = df_sol.z[i]
-			xlabel = "position z in m"
-		elseif xx=="t"
-			xvalues[i] = df_sol.t[i]
-			xlabel = "time t in s"
-		elseif xx=="T"
-			xvalues[i] = par.prog.T_itp.(df_sol.z[i], df_sol.t[i]).-273.15
-			xlabel = "temperature T in °C"
-		elseif xx=="τ"
-			xvalues[i] = sqrt.(df_sol.τ²[i])
-			xlabel = "peak width τ in s"
-		elseif xx=="σ"
-			xvalues[i] = velocity(df_sol, i, par).*sqrt.(df_sol.τ²[i])
-			xlabel = "band width in m"
-		elseif xx=="u"
-			xvalues[i] = velocity(df_sol, i, par)
-			xlabel = "solute velocity in m/s"
-		end
-		if yy=="z"
-			yvalues[i] = df_sol.z[i]
-			ylabel = "position z in m"
-		elseif yy=="t"
-			yvalues[i] = df_sol.t[i]
-			ylabel = "time t in s"
-		elseif yy=="T"
-			yvalues[i] = par.prog.T_itp.(df_sol.z[i], df_sol.t[i]).-273.15
-			ylabel = "temperature T in °C"
-		elseif yy=="τ"
-			yvalues[i] = sqrt.(df_sol.τ²[i])
-			ylabel = "peak width in s"
-		elseif yy=="σ"
-			yvalues[i] = velocity(df_sol, i, par).*sqrt.(df_sol.τ²[i])
-			ylabel = "band width in m"
-		elseif yy=="u"
-			yvalues[i] = velocity(df_sol, i, par)
-			ylabel = "solute velocity in m/s"
-		end
-		plot!(p_add, xvalues[i], yvalues[i], xlabel=xlabel, ylabel=ylabel, label=par.sub[i].name, m=:o)
-	end
-	return p_add
-end
-
-function velocity(df_sol, i, par)
-	x = df_sol.z[i]
-	t = df_sol.t[i]
-	T_itp = par.prog.T_itp
-	pin_itp = par.prog.pin_itp
-	pout_itp = par.prog.pout_itp
-	L = par.sys.L
-	d = par.sys.d
-	df = par.sys.df
-	gas = par.sys.gas
-	ΔCp = par.sub[i].ΔCp
-	Tchar = par.sub[i].Tchar
-	θchar = par.sub[i].θchar
-	φ₀ = par.sub[i].φ₀
-	u = Array{Float64}(undef, length(x))
-	for j=1:length(x)
-		u[j] = 1/GasChromatographySimulator.residency(x[j], t[j], T_itp, pin_itp, pout_itp, L, d, df, gas, ΔCp, Tchar, θchar, φ₀)
-	end
-	return u
-end
-	md"""
-	Definition of more functions.
-	"""
-end
-
 # ╔═╡ a2287fe8-5aa2-4259-bf7c-f715cc866243
 begin
 	plotly()
@@ -368,7 +165,7 @@ end
 # ╔═╡ 0740f2e6-bce0-4590-acf1-ad4d7cb7c523
 begin
 	plotly()
-	add_plots(xx, yy, solution, par)
+	GasChromatographyTools.local_plots(xx, yy, solution, par)
 end
 
 # ╔═╡ 95e1ca30-9442-4f39-9af0-34bd202fcc24
@@ -377,27 +174,25 @@ md"""
 """
 
 # ╔═╡ Cell order:
-# ╟─115b320f-be42-4116-a40a-9cf1b55d39b5
+# ╠═115b320f-be42-4116-a40a-9cf1b55d39b5
 # ╟─9c54bef9-5b70-4cf7-b110-a2f48f5db066
 # ╟─c9246396-3c01-4a36-bc9c-4ed72fd9e325
 # ╟─8b3011fd-f3df-4ab0-b611-b943d5f3d470
-# ╟─273dcf96-6de4-4380-a00f-ed119bfa13b7
-# ╟─e0669a58-d5ac-4d01-b079-05412b413dda
-# ╟─a7e1f0ee-714e-4b97-8741-d4ab5321d5e0
-# ╟─7a00bb54-553f-47f5-b5db-b40d226f4183
-# ╟─3e053ac1-db7b-47c1-b52c-00e26b59912f
+# ╠═273dcf96-6de4-4380-a00f-ed119bfa13b7
+# ╠═e0669a58-d5ac-4d01-b079-05412b413dda
+# ╠═a7e1f0ee-714e-4b97-8741-d4ab5321d5e0
+# ╠═7a00bb54-553f-47f5-b5db-b40d226f4183
+# ╠═3e053ac1-db7b-47c1-b52c-00e26b59912f
 # ╟─323a769f-55f9-41dd-b8f1-db7928996a52
 # ╟─fdb39284-201b-432f-bff6-986ddbc49a7d
-# ╟─49faa7ea-0f22-45ca-9ab5-338d0db25564
+# ╠═49faa7ea-0f22-45ca-9ab5-338d0db25564
 # ╟─14db2d66-eea6-43b1-9caf-2039709d1ddb
 # ╟─a2287fe8-5aa2-4259-bf7c-f715cc866243
 # ╟─3c856d47-c6c2-40d3-b547-843f9654f48d
-# ╟─0740f2e6-bce0-4590-acf1-ad4d7cb7c523
-# ╟─802e4071-b22b-4411-b589-205292aabc75
-# ╟─48f91bc4-35ce-470a-9b6d-eb3c08da27dc
-# ╟─f7f06be1-c8fa-4eee-953f-0d5ea26fafbf
-# ╟─ee267b33-4086-4e04-9f39-b7f53f2ec920
-# ╟─e3277bb4-301a-4a1e-a838-311832b6d6aa
-# ╟─115fa61e-8e82-42b2-8eea-9c7e21d97ea8
-# ╟─85954bdb-d649-4772-a1cd-0bda5d9917e9
+# ╠═0740f2e6-bce0-4590-acf1-ad4d7cb7c523
+# ╠═f7f06be1-c8fa-4eee-953f-0d5ea26fafbf
+# ╠═ee267b33-4086-4e04-9f39-b7f53f2ec920
+# ╠═e3277bb4-301a-4a1e-a838-311832b6d6aa
+# ╠═115fa61e-8e82-42b2-8eea-9c7e21d97ea8
+# ╠═85954bdb-d649-4772-a1cd-0bda5d9917e9
 # ╟─95e1ca30-9442-4f39-9af0-34bd202fcc24
