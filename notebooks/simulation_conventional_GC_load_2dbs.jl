230d5341522e21131dfb4cbe066b7d89a82c4bc7
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2022-02-01 13:01:28 +0100

    Update simulation_conventional_GC_load_2dbs.jl

--- notebooks/simulation_conventional_GC_load_2dbs.jl
+++ notebooks/simulation_conventional_GC_load_2dbs.jl
@@ -66,28 +66,53 @@ second database: $(@bind db_file_2 FilePicker())
 """
 
 # ╔═╡ c7c5da86-57ad-4a35-bbaa-39871ee3205d
-begin
+if typeof(db_file_1) == Nothing
+	md"""
+	#### !!!Attention!!!
+	**Select a 1st database.**
+	"""
+else
 	db_1 = DataFrame(CSV.File(db_file_1["data"]))
-	sp_1 = unique(db_1.Phase)
-	db_2 = DataFrame(CSV.File(db_file_2["data"]))
-	sp_2 = unique(db_2.Phase)
-	sp = GasChromatographyTools.common(sp_1, sp_2)
 	md"""
 	1st File: $(db_file_1["name"])
 	$(embed_display(db_1))
+	"""
+end
 
+# ╔═╡ 1b827021-05a6-4fc8-bdce-31302f4a5d0f
+if typeof(db_file_2) == Nothing
+	md"""
+	#### !!!Attention!!!
+	**Select a 2nd database.**
+	"""
+else
+	db_2 = DataFrame(CSV.File(db_file_2["data"]))
+	md"""
 	2nd File: $(db_file_2["name"])
 	$(embed_display(db_2))
 	"""
 end
 
 # ╔═╡ 59e7dff6-7db0-4262-b4f5-ffe8f1a28542
-begin
+if (@isdefined db_1) && (@isdefined db_2)
+	sp_1 = unique(db_1.Phase)
+	sp_2 = unique(db_2.Phase)
+	sp = GasChromatographyTools.common(sp_1, sp_2)
 	common_db = innerjoin(db_1[!, [:Name, :Phase, :Tchar, :thetachar, :DeltaCp, :phi0]], db_2[!, [:Name, :Phase, :Tchar, :thetachar, :DeltaCp, :phi0]], on=[:Name, :Phase], makeunique=true)
 	rename!(common_db, [:Name, :Phase, :Tchar_1, :θchar_1, :ΔCp_1, :φ₀_1, :Tchar_2, :θchar_2, :ΔCp_2, :φ₀_2])
 	md"""
 	Show the common (name/stationary phases) database:
 	$(embed_display(common_db))
+	"""
+elseif @isdefined db_1
+	sp = unique(db_1.Phase)
+	md"""
+	Only data from the 1st database is used.
+	"""
+elseif @isdefined db_2
+	sp = unique(db_2.Phase)
+	md"""
+	Only data from the 2nd database is used.
 	"""
 end
 
@@ -97,82 +122,204 @@ end
 # ╔═╡ a7e1f0ee-714e-4b97-8741-d4ab5321d5e0
 @bind prog_values confirm(GasChromatographyTools.UI_Program_ng())
 
-# ╔═╡ 323a769f-55f9-41dd-b8f1-db7928996a52
+# ╔═╡ 603e0868-dda5-4063-872f-ce966b6fa8ad
 md"""
-## Plot of the program
+### Add measured retention times
 
-select temperature plot: $(@bind Tplot Select(["T(x,t)", "T(x)", "T(t)"]; default="T(t)"))
+$(@bind meas_rt_file FilePicker())
 """
+
+# ╔═╡ 5e17ec20-13a1-4f87-a99c-43029907ab9c
+if typeof(meas_rt_file) != Nothing
+	meas_rt = DataFrame(CSV.File(meas_rt_file["data"]))
+	md"""
+	Measured retention data:
+	
+	$(embed_display(meas_rt))
+	"""
+end
 
 # ╔═╡ 3c856d47-c6c2-40d3-b547-843f9654f48d
 md"""
 ### Plot of local values
 
-Plot $(@bind yy Select(["z", "t", "T", "τ", "σ", "u"]; default="t")) over $(@bind xx Select(["z", "t", "T", "τ", "σ", "u"]; default="z"))
+Plot $(@bind yy Select(["z", "t", "T", "τ", "σ", "u"]; default="t")) over $(@bind xx Select(["z", "t"]; default="z"))
 """
 
 # ╔═╡ f7f06be1-c8fa-4eee-953f-0d5ea26fafbf
 sys = GasChromatographySimulator.System(sys_values[1], sys_values[2]*1e-3, sys_values[3]*1e-6, sys_values[4], sys_values[5]);
 
 # ╔═╡ 7a00bb54-553f-47f5-b5db-b40d226f4183
-@bind sub_values confirm(GasChromatographyTools.UI_Substance_name(GasChromatographyTools.common(GasChromatographySimulator.all_solutes(sys.sp, db_1), GasChromatographySimulator.all_solutes(sys.sp, db_2))))
+begin
+	if (@isdefined db_1) && (@isdefined db_2)
+		sub_names = GasChromatographyTools.common(
+							GasChromatographySimulator.all_solutes(sys.sp, db_1), 								GasChromatographySimulator.all_solutes(sys.sp, db_2)
+							)
+	elseif @isdefined db_1
+		sub_names = GasChromatographySimulator.all_solutes(sys.sp, db_1)
+	elseif @isdefined db_2
+		sub_names = GasChromatographySimulator.all_solutes(sys.sp, db_2)
+	end
+	@bind sub_values confirm(GasChromatographyTools.UI_Substance_name(sub_names))
+end
 
 # ╔═╡ 0bb1bc3e-9c23-4fbd-9872-fe2e4a2dbdea
 prog = GasChromatographyTools.setting_prog(prog_values, sys.L);
 
 # ╔═╡ e3277bb4-301a-4a1e-a838-311832b6d6aa
-sub_1 = GasChromatographySimulator.load_solute_database(db_1, sys.sp, sys.gas, sub_values[1], zeros(length(sub_values[1])), zeros(length(sub_values[1])));
+if @isdefined db_1
+	sub_1 = GasChromatographySimulator.load_solute_database(db_1, sys.sp, sys.gas, sub_values[1], zeros(length(sub_values[1])), zeros(length(sub_values[1])))
+end;
 
 # ╔═╡ d201c020-9311-418d-a652-200667d2d64e
-sub_2 = GasChromatographySimulator.load_solute_database(db_2, sys.sp, sys.gas, sub_values[1], zeros(length(sub_values[1])), zeros(length(sub_values[1])));
+if @isdefined db_2
+	sub_2 = GasChromatographySimulator.load_solute_database(db_2, sys.sp, sys.gas, sub_values[1], zeros(length(sub_values[1])), zeros(length(sub_values[1])))
+end;
 
 # ╔═╡ 115fa61e-8e82-42b2-8eea-9c7e21d97ea8
 opt = GasChromatographySimulator.Options(;abstol=1e-8, reltol=1e-5);
 
 # ╔═╡ 85954bdb-d649-4772-a1cd-0bda5d9917e9
-par_1 = GasChromatographySimulator.Parameters(sys, prog, sub_1, opt);
+if @isdefined db_1
+	par_1 = GasChromatographySimulator.Parameters(sys, prog, sub_1, opt)
+end;
+
+# ╔═╡ 63d59b94-bea6-4a9a-a0f3-e97602ceeb7e
+if @isdefined db_2
+	par_2 = GasChromatographySimulator.Parameters(sys, prog, sub_2, opt)
+end;
 
 # ╔═╡ fdb39284-201b-432f-bff6-986ddbc49a7d
 begin
 	gr()
-	plot_T = GasChromatographySimulator.plot_temperature(par_1; selector=Tplot)
-	if Tplot=="T(x)"
-		plot!(plot_T, legend=:bottomleft)
+	if (@isdefined db_1) && (@isdefined db_2)
+		par = par_1
+	elseif @isdefined db_1
+		par = par_1
+	elseif @isdefined db_2
+		par = par_2
 	end
-	plot_p = GasChromatographySimulator.plot_pressure(par_1.prog)
+	plot_T = GasChromatographySimulator.plot_temperature(par; selector="T(t)")
+	plot_p = GasChromatographySimulator.plot_pressure(par.prog)
 	xlabel!(plot_p, "")
-	plot_F = GasChromatographySimulator.plot_flow(par_1)
+	plot_F = GasChromatographySimulator.plot_flow(par)
 	l = @layout([a{0.65w} [b; c]])
 	p_TpF = plot(plot_T, plot_p, plot_F, layout=l, size=(620,300))
 	md"""
+	## Plot of the program
+	
 	$(embed_display(p_TpF))
 	"""
 end
 
-# ╔═╡ 63d59b94-bea6-4a9a-a0f3-e97602ceeb7e
-par_2 = GasChromatographySimulator.Parameters(sys, prog, sub_2, opt);
-
 # ╔═╡ 49faa7ea-0f22-45ca-9ab5-338d0db25564
-begin	
-	peaklist_1, solution_1 = GasChromatographySimulator.simulate(par_1)
-	peaklist_2, solution_2 = GasChromatographySimulator.simulate(par_2)
+begin
+	if @isdefined db_1
+		peaklist_1, solution_1 = GasChromatographySimulator.simulate(par_1)
+	end
+	if @isdefined db_2
+		peaklist_2, solution_2 = GasChromatographySimulator.simulate(par_2)
+	end
 	md"""
 	## Simulation
 	"""
 end
 
 # ╔═╡ 14db2d66-eea6-43b1-9caf-2039709d1ddb
-# new common peaklist name tR1 tR2 DtR tauR1 tauR2 DtauR 
-md"""
-### Peaklist
-$(embed_display(GasChromatographyTools.common_peaklist(peaklist_1, peaklist_2)))
-"""
+if (@isdefined db_1) && (@isdefined db_2)
+	md"""
+	### Peaklist
+	Comparison of simulation with both databases:
+	$(embed_display(GasChromatographyTools.compare_peaklist(peaklist_1, peaklist_2)))
+	"""
+elseif @isdefined db_1
+	md"""
+	### Peaklist
+	$(embed_display(peaklist_1))
+	"""
+elseif @isdefined db_2
+	md"""
+	### Peaklist
+	$(embed_display(peaklist_2))
+	"""
+end
+
+# ╔═╡ 9c1e5bc5-8396-4caf-bb80-fe89fea0a5c5
+begin
+	if @isdefined meas_rt
+		if (@isdefined db_1) && (@isdefined db_2)
+			compare_1 = GasChromatographyTools.compare_measurement_simulation(meas_rt, peaklist_1)
+			compare_2 = GasChromatographyTools.compare_measurement_simulation(meas_rt, peaklist_2)
+			RSS_1 = sum(compare_1.ΔtR[isnan.(compare_1.ΔtR).==false].^2)
+			RSS_2 = sum(compare_2.ΔtR[isnan.(compare_2.ΔtR).==false].^2)
+			md"""
+			### Measurement vs. Simulation
+			Compare measured retention times with simulation of 1st database:
+			$(embed_display(compare_1))
+			
+			Compare measured retention times with simulation of 2nd database:
+			$(embed_display(compare_2))
+		
+			The residual sum of squares are:
+			- 1st database: $(round(RSS_1; digits=2)) s²
+			- 2nd database: $(round(RSS_2; digits=2)) s²
+			"""
+		elseif @isdefined db_1
+			compare_1 = GasChromatographyTools.compare_measurement_simulation(meas_rt, peaklist_1)
+			RSS_1 = sum(compare_1.ΔtR[isnan.(compare_1.ΔtR).==false].^2)
+			md"""
+			### Measurement vs. Simulation
+			Compare measured retention times with simulation of 1st database:
+			$(embed_display(compare_1))
+			
+			The residual sum of squares is:
+			$(round(RSS_1; digits=2)) s²
+			"""
+		elseif @isdefined db_2
+			compare_2 = GasChromatographyTools.compare_measurement_simulation(meas_rt, peaklist_2)
+			RSS_2 = sum(compare_2.ΔtR[isnan.(compare_2.ΔtR).==false].^2)
+			md"""
+			### Measurement vs. Simulation
+			Compare measured retention times with simulation of 2nd database:
+			$(embed_display(compare_2))
+			
+			The residual sum of squares is:
+			$(round(RSS_2; digits=2)) s²
+			"""
+		end
+	end
+end
 
 # ╔═╡ a2287fe8-5aa2-4259-bf7c-f715cc866243
 begin
 	plotly()
-	pchrom = GasChromatographySimulator.plot_chromatogram(peaklist_1, (0,sum(par_1.prog.time_steps)))[1]
-	GasChromatographySimulator.plot_chromatogram!(pchrom, peaklist_2, (0,sum(par_2.prog.time_steps)); mirror=true)
+	if (@isdefined db_1) && (@isdefined db_2)
+		pchrom = GasChromatographySimulator.plot_chromatogram(peaklist_1, (0,sum(par_1.prog.time_steps)))[1]
+		GasChromatographySimulator.plot_chromatogram!(pchrom, peaklist_2, (0,sum(par_2.prog.time_steps)); mirror=true)
+		pchrom[1][1][:label] = "1st database"
+		pchrom[1][2][:label] = "2nd database"
+		ymax = maximum(pchrom[1][1][:y])*1.1
+		ymin = minimum(pchrom[1][2][:y])*1.1
+	elseif @isdefined db_1
+		pchrom = GasChromatographySimulator.plot_chromatogram(peaklist_1, (0,sum(par_1.prog.time_steps)))[1]
+		pchrom[1][1][:label] = "1st database"
+		ymax = maximum(pchrom[1][1][:y])*1.1
+		ymin = 0.0
+	elseif @isdefined db_2
+		pchrom = GasChromatographySimulator.plot_chromatogram(peaklist_2, (0,sum(par_2.prog.time_steps)))[1]
+		pchrom[1][1][:label] = "2nd database"
+		ymax = maximum(pchrom[1][1][:y])*1.1
+		ymin = 0.0
+	end
+	# add measured data
+	if @isdefined meas_rt
+		for i=1:length(meas_rt.RT)
+			plot!(pchrom, meas_rt.RT[i].*ones(2), [ymin, ymax], c=:orange, line=:solid, label=meas_rt.Name[i])
+			annotate!(meas_rt.RT[i], ymax, Plots.text.(meas_rt.Name[i], 10, :orange))
+		end
+	end
+	# finalize
+	plot!(pchrom, legend=true, size=(680,400))
 	md"""
 	### Chromatogram
 
@@ -183,9 +330,22 @@ end
 # ╔═╡ 0740f2e6-bce0-4590-acf1-ad4d7cb7c523
 begin
 	plotly()
-	p_1 = GasChromatographyTools.local_plots(xx, yy, solution_1, par_1)
-	p_2 = GasChromatographyTools.local_plots(xx, yy, solution_2, par_2)
-	p_add = plot(p_1, p_2)
+	if (@isdefined db_1) && (@isdefined db_2)
+		p_1 = GasChromatographyTools.local_plots(xx, yy, solution_1, par_1)
+		plot!(p_1, title="1st database")
+		p_2 = GasChromatographyTools.local_plots(xx, yy, solution_2, par_2)
+		plot!(p_2, title="2nd database")
+		p_local = plot(p_1, p_2)
+	elseif @isdefined db_1
+		p_local = GasChromatographyTools.local_plots(xx, yy, solution_1, par_1)
+		plot!(p_local, title="1st database")
+	elseif @isdefined db_2
+		p_local = GasChromatographyTools.local_plots(xx, yy, solution_2, par_2)
+		plot!(p_local, title="2nd database")
+	end
+	md"""
+	$(embed_display(p_local))
+	"""
 end
 
 # ╔═╡ 95e1ca30-9442-4f39-9af0-34bd202fcc24
@@ -198,19 +358,22 @@ md"""
 # ╟─9c54bef9-5b70-4cf7-b110-a2f48f5db066
 # ╟─c9246396-3c01-4a36-bc9c-4ed72fd9e325
 # ╟─8b3011fd-f3df-4ab0-b611-b943d5f3d470
-# ╟─84b0de13-2653-4e68-abf4-d98830809844
-# ╠═c7c5da86-57ad-4a35-bbaa-39871ee3205d
+# ╠═84b0de13-2653-4e68-abf4-d98830809844
+# ╟─c7c5da86-57ad-4a35-bbaa-39871ee3205d
+# ╟─1b827021-05a6-4fc8-bdce-31302f4a5d0f
 # ╟─59e7dff6-7db0-4262-b4f5-ffe8f1a28542
-# ╠═e0669a58-d5ac-4d01-b079-05412b413dda
-# ╠═a7e1f0ee-714e-4b97-8741-d4ab5321d5e0
-# ╠═7a00bb54-553f-47f5-b5db-b40d226f4183
-# ╟─323a769f-55f9-41dd-b8f1-db7928996a52
+# ╟─e0669a58-d5ac-4d01-b079-05412b413dda
+# ╟─a7e1f0ee-714e-4b97-8741-d4ab5321d5e0
+# ╟─7a00bb54-553f-47f5-b5db-b40d226f4183
+# ╟─603e0868-dda5-4063-872f-ce966b6fa8ad
+# ╟─5e17ec20-13a1-4f87-a99c-43029907ab9c
 # ╟─fdb39284-201b-432f-bff6-986ddbc49a7d
 # ╟─49faa7ea-0f22-45ca-9ab5-338d0db25564
 # ╟─14db2d66-eea6-43b1-9caf-2039709d1ddb
 # ╟─a2287fe8-5aa2-4259-bf7c-f715cc866243
+# ╟─9c1e5bc5-8396-4caf-bb80-fe89fea0a5c5
 # ╟─3c856d47-c6c2-40d3-b547-843f9654f48d
-# ╠═0740f2e6-bce0-4590-acf1-ad4d7cb7c523
+# ╟─0740f2e6-bce0-4590-acf1-ad4d7cb7c523
 # ╠═f7f06be1-c8fa-4eee-953f-0d5ea26fafbf
 # ╠═0bb1bc3e-9c23-4fbd-9872-fe2e4a2dbdea
 # ╠═e3277bb4-301a-4a1e-a838-311832b6d6aa
