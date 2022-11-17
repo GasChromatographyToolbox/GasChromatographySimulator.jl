7bbf519d82491862a9ea9ce6e1add868fa93c0fe
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2022-11-17 14:55:55 +0100

    Update simulation_example.jl

--- notebooks/simulation_example.jl
+++ notebooks/simulation_example.jl
@@ -18,7 +18,7 @@ end
 begin
     using CSV, DataFrames,  GasChromatographySimulator, HypertextLiteral, Plots, PlutoUI, UrlDownload
 	md"""
-	Packages, simulation\_example.jl, for GasChromatographySimulator v0.3.11
+	Packages, simulation\_example.jl, for GasChromatographySimulator v0.3.14
 	"""
 end
 
