228285d4b72de0a20ca2fd56e04978cedd9d68c5
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2023-07-06 12:27:56 +0200

    Update GasChromatographySimulator.jl

--- src/GasChromatographySimulator.jl
+++ src/GasChromatographySimulator.jl
@@ -1481,15 +1481,15 @@ function peaklist(sol, par)
             Annotations[i] = join(split(par.sub[i].ann, ", ")[1:end-1], ", ")
         end
     end  
-    df = sort!(DataFrame(No = No, Name = Name, CAS = CAS, tR = tR, τR = τR, TR=TR, σR = σR, uR = uR, kR = kR, ), [:tR])
+    df = sort!(DataFrame(No = No, Name = Name, CAS = CAS, tR = tR, τR = τR, TR=TR, σR = σR, uR = uR, kR = kR, Annotations = Annotations, ), [:tR])
     Threads.@threads for i=1:n-1
         Res[i] = (df.tR[i+1] - df.tR[i])/(2*(df.τR[i+1] + df.τR[i]))
         Δs[i] = (df.tR[i+1] - df.tR[i])/(df.τR[i+1] - df.τR[i]) * log(df.τR[i+1]/df.τR[i])
     end
     df[!, :Res] = Res
     df[!, :Δs] = Δs 
-    df[!, :Annotations] = Annotations 
-    return df
+    
+    return select(df, [:No, :Name, :CAS, :tR, :τR, :TR, :σR, :uR, :kR, :Res, :Δs, :Annotations])
 end
 
 """
@@ -1563,15 +1563,15 @@ function peaklist(sol, peak, par)
             Annotations[i] = join(split(par.sub[i].ann, ", ")[1:end-1], ", ")
         end
     end  
-    df = sort!(DataFrame(No = No, Name = Name, CAS = CAS, tR = tR, τR = τR, TR=TR, σR = σR, uR = uR, kR = kR, ), [:tR])
+    df = sort!(DataFrame(No = No, Name = Name, CAS = CAS, tR = tR, τR = τR, TR=TR, σR = σR, uR = uR, kR = kR, Annotations = Annotations, ), [:tR])
     Threads.@threads for i=1:n-1
         Res[i] = (df.tR[i+1] - df.tR[i])/(2*(df.τR[i+1] + df.τR[i]))
         Δs[i] = (df.tR[i+1] - df.tR[i])/(df.τR[i+1] - df.τR[i]) * log(df.τR[i+1]/df.τR[i])
     end
     df[!, :Res] = Res 
     df[!, :Δs] = Δs   
-    df[!, :Annotations] = Annotations 
-    return df
+    
+    return select(df, [:No, :Name, :CAS, :tR, :τR, :TR, :σR, :uR, :kR, :Res, :Δs, :Annotations])
 end
 
 """
