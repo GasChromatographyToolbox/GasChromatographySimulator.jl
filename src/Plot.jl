f4acabdc2f9d9521e6037ef2692b79b2e141d7ac
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2023-08-08 14:54:47 +0200

    Update Plot.jl
    - fix

--- src/Plot.jl
+++ src/Plot.jl
@@ -4,9 +4,9 @@
 
 Calculate the chromatogram as a sum of gaussian peaks over the time `t` for peaks centered at retention times `tR` and with peak width `τR`.    
 """
-function chromatogram(t::Array{Float64,1}, tR::Array{Float64,1}, τR::Array{Float64,1})
+function chromatogram(t::Array{Real,1}, tR::Array{Real,1}, τR::Array{Real,1})
 	g(t,tR,τR) = 1/sqrt(2*π*τR^2)*exp(-(t-tR)^2/(2*τR^2))
-	chromatograms = Array{Array{Float64,1}}(undef, length(tR))
+	chromatograms = Array{Array{Real,1}}(undef, length(tR))
 	for j=1:length(tR)
 		chromatograms[j] = g.(t, tR[j], τR[j])
 	end
