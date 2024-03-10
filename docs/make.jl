9fedd464adb55324bd3d225cca24af6ff4042ea8
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2024-03-10 12:24:35 +0100

    fix documentation

--- docs/make.jl
+++ docs/make.jl
@@ -17,6 +17,6 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/JanLeppert/GasChromatographySimulator.jl",
+    repo = "github.com/GasChromatographyToolbox/GasChromatographySimulator.jl",
     devbranch = "main"
 )
